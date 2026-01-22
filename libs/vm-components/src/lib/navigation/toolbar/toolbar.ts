f4808f792d0fde26660b1167a84ce920059d0151
Florian Arndt <far@connext.de>
2026-01-22 16:15:56 +0100

    fixing toolbar

--- libs/vm-components/src/lib/navigation/toolbar/toolbar.ts
+++ libs/vm-components/src/lib/navigation/toolbar/toolbar.ts
@@ -1,6 +1,7 @@
-import {Component, input, InputSignal} from '@angular/core';
+import {Component, inject, input, InputSignal} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
+import {Router} from '@angular/router';
 
 export interface IToolbarItem {
   name: string;
@@ -11,13 +12,18 @@ export interface IToolbarItem {
 @Component({
   selector: 'vmc-toolbar',
   imports: [
-    MatToolbar,
-    MatIcon
+    MatToolbar
   ],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
 export class Toolbar {
+  readonly #router = inject(Router);
+
   applicationName: InputSignal<string> = input.required();
   toolbarItems: InputSignal<IToolbarItem[]> = input.required();
+
+  async redirect(url: string) {
+    await this.#router.navigate([url]);
+  }
 }
