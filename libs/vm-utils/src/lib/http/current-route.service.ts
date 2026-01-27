f5fdc28b9fa68afa7c529eb4198b15ccaee5d4c3
Florian Arndt <far@connext.de>
2026-01-27 12:27:11 +0100

    [Feature] Implement admin layout and routing with sidebar and navbar components -- 2

---
+++ libs/vm-utils/src/lib/http/current-route.service.ts
@@ -0,0 +1,27 @@
+import {inject, Injectable} from '@angular/core';
+import {BehaviorSubject, filter, shareReplay} from 'rxjs';
+import {EventType, Router} from '@angular/router';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrentRouteService {
+  readonly #router = inject(Router);
+
+  // da der Router kein Observable für die URL änderungen bereitstellt, hier ein BehaviorSubject als workaround.
+  #route$ = new BehaviorSubject(this.#router.url);
+
+  route$ = this.#route$
+    .asObservable()
+    .pipe(shareReplay({ bufferSize: 1, refCount: false }));
+
+  constructor() {
+    this.#router.events
+      .pipe(
+        filter(event => event.type === EventType.NavigationEnd),
+        takeUntilDestroyed())
+      .subscribe(() => this.#route$.next(this.#router.url)
+      );
+  }
+}
