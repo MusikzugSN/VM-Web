40d1f86dc955eda73d67e14dd6be0be35c88dddd
Florian Arndt <far@connext.de>
2026-01-27 15:32:45 +0100

    fix sidebar styles

--- libs/vm-components/src/lib/navigation/sidebar/vmc-sidebar.component.ts
+++ libs/vm-components/src/lib/navigation/sidebar/vmc-sidebar.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject, input, InputSignal} from '@angular/core';
 import {Router} from '@angular/router';
 import {VmcExtentionPannel} from '../../layout/extentionPanel/vmc-extention-pannel.component';
+import {VmcToolbar} from '../../layout/toolbar/vmc-toolbar.component';
 
 export interface ISidebarGroup {
   groupName: string;
@@ -17,7 +18,8 @@ export interface ISidebarItem {
 @Component({
   selector: 'vmc-sidebar',
   imports: [
-    VmcExtentionPannel
+    VmcExtentionPannel,
+    VmcToolbar
   ],
   templateUrl: './vmc-sidebar.component.html',
   styleUrl: './vmc-sidebar.component.scss',
